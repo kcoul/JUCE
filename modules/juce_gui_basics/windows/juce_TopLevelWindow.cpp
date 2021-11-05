8d860c538eb4507320468d4e908bbd20deb01b69
kcoul <kieran.coulter@gmail.com>
2021-11-04 19:05:32 -0700

    Test workaround for root remote debugging affecting windowing

--- modules/juce_gui_basics/windows/juce_TopLevelWindow.cpp
+++ modules/juce_gui_basics/windows/juce_TopLevelWindow.cpp
@@ -138,6 +138,8 @@ TopLevelWindow::TopLevelWindow (const String& name, const bool shouldAddToDeskto
 
     setOpaque (true);
 
+    centreWithSize(800, 480);
+
     if (shouldAddToDesktop)
         Component::addToDesktop (TopLevelWindow::getDesktopWindowStyleFlags());
     else
